f479ae02ab95ba2be48d16ad621e98132d4efe0b
bcliffor <benjaminmclifford@gmail.com>
2012-08-08 19:30:53 -0400

    added final version of non-packaged library

--- highlight.js
+++ highlight.js
@@ -1,39 +1,28 @@
-  
+(function($){
   // jQuery function to wrap in span tags with the passed in class any text that
   // matches the passed in text. Will only occur within the element we called the
   // function on.
-  jQuery.fn.highlight = function (str, className) {     
-    //  setup regular expression with options gi to match all searches and be case insensitive     
-    var regexNoCase = new RegExp(str, "gi");
-    //  loop through all instances of object that we were called on
-    return this.each(function () {
-        //  get the contents of the element, which means all children including text nodes. then filter...
+  $.fn.highlight = function (str, className) {     
+    // Setup regular expression with options gi to match all searches and be case insensitive     
+    var regex = new RegExp(str, "gi");
+    this.data('previousHtml',this.html());
+    // Get all elements (this and children) of the calling object
+    return this.find('*').andSelf().each(function () {
+        // Get the contents of the element, which means all children including text nodes. then filter...
         $(this).contents().filter(function () {
-           //only perform the next operation on an element if it is type 3 (text), and matches our regex
-            return this.nodeType == 3 && regexNoCase.test(this.nodeValue);
+           // Only perform the next operation on an element if it is type 3 (text), and matches our regex
+            return this.nodeType === 3 && regex.test(this.nodeValue);
         })
+          // If we passed then replace all occurences of the matching text with a span class = "highlighted"
           .replaceWith(function () {
-            
-            // get all of the unique matches to then search for globally with case
-            var matches = $.unique(this.nodeValue.match(regexNoCase));
-            var replacements = [];
-
-            //get a proper regex replacement string to match each case of the word found
-            for (var val in matches){
-              replacements[val] = "<span class=\"" + className + "\">" + matches[val] + "</span>";
-            }            
-            
-            // for each unique case, replace all of its occurences case sensitively
-            for( var val in matches){
-                var regexCase = new RegExp(matches[val], "g");
-                (this.nodeValue) = (this.nodeValue).replace(regexCase, replacements[val]);
-            }
-
-            return this.nodeValue;
-
-            //var x =(this.nodeValue).replace(regex, "<span class=\"" + className + "\">" + str + "</span>");
-            //return x;
+            return (this.nodeValue).replace(regex, "<span class=\"" + className + "\">$&</span>");
         });
     });  
   };
-});
+  
+  // simple restore of previous html
+  $.fn.removeHighlight = function () {
+    this.html(this.data('previousHtml'));
+  };
+
+})(jQuery);
